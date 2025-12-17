aa6704b9d621595a0f349963d720af4a1838c919
naveen841 <79522681+naveen841@users.noreply.github.com>
2025-12-17 22:48:57 +0530

    Jenkinsfile

--- arth.sh
+++ arth.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-
-let a=b+c
-
-let s=b-c
-
-print(a ,s)
+pipeline {
+  agent {
+    docker { image 'node:16-alpine' }
+  }
+  stages {
+    stage('Test') {
+      steps {
+        sh 'node --version'
+      }
+    }
+  }
+}
